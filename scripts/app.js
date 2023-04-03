79f1b948d80c42e0bf223cecb883aab67cf43fe1
smids <dimas.setiawan92@gmail.com>
2023-04-03 14:17:31 +0700

    showing & hiding player modal

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+const playerOverlayElement = document.getElementById('config-overlay');
+const backdropElement = document.getElementById('backdrop');
+
+const editPlayer1Btn = document.getElementById('edit-player-1-btn');
+const editPlayer2Btn = document.getElementById('edit-player-2-btn');
+const cancelPlayerBtn = document.getElementById('cancel-config-btn');
+
+editPlayer1Btn.addEventListener('click',openPlayerConfig);
+editPlayer2Btn.addEventListener('click',openPlayerConfig);
+cancelPlayerBtn.addEventListener('click', closePlayerConfig);
+backdropElement.addEventListener('click', closePlayerConfig);
---
+++ scripts/config.js
@@ -0,0 +1,9 @@
+function openPlayerConfig(){
+    playerOverlayElement.style.display = 'block';
+    backdropElement.style.display = 'block';
+}
+
+function closePlayerConfig(){
+    playerOverlayElement.style.display = 'none';
+    backdropElement.style.display = 'none';
+}
